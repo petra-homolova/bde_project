44272bea1c8510d4099a6a2e7bde7cf141c11c55
“petra-homolova” <petra.homolova26@gmail.com>
2024-11-03 19:29:22 +0100

    Delivery

--- test/todo-app.js
+++ test/todo-app.js
@@ -1,4 +1,25 @@
-import { Selector } from "testcafe";
+import {Selector} from "testcafe";
 
-fixture("ToDo app tests")
-    .page("http://localhost:5173/")
+fixture("Wordpress test")
+.page("http://good-health-and-well-being.local/wp-admin/")
+
+test("Valid login", async t => {
+    
+    await t
+        .expect(Selector("#user_login").exists).ok("Login input not found")
+        .typeText(Selector("#user_login"), "Petra")
+        .typeText(Selector("#user_pass"), "smudlicky1414")
+        .click(Selector("#wp-submit"))
+        .expect(Selector("body").hasClass("wp-admin")).eql(true);
+
+})
+
+test("Invalid login", async t => {
+    
+    await t
+        .typeText(Selector("#user_login"), "invalid")
+        .typeText(Selector("#user_pass"), "invalidpass")
+        .click(Selector("#wp-submit"))
+        .expect(Selector("body").hasClass("wp-admin")).eql(false);
+
+})
